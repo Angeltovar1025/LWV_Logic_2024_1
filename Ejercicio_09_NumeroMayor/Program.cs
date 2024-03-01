92e0056fa090b9b5694a2346eeeec4caee6b2791
Equipo 012 <Equipo 012@DESKTOP-F5GRAV0>
2024-03-01 09:16:06 -0500

    Solucion Ejercicio_09_NumeroMayor

---
+++ Ejercicio_09_NumeroMayor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Se declaran variables
+        Double Num1, Num2, Num3;
+
+        //
+         Console.Write("Digite el primer numero: ");
+        Num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        Console.Write("Digite el segundo numero: ");
+        Num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        Console.Write("Digite el tercer numero: ");
+        Num3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        //Validacion
+        if ((Num1 > Num2) && (Num1 > Num3))
+
+            Console.WriteLine("El numero mayor es: " + Num1);
+
+        else 
+            if ((Num2 > Num3))
+
+        Console.WriteLine("El numero mayor es: " + Num2);
+        else 
+            Console.WriteLine("El numero mayor es: " + Num3);
+
+    }
+}
