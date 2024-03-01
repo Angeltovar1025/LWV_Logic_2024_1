051fb8f9385f00fb2437877cc1451ae4c91c55bc
Equipo 012 <Equipo 012@DESKTOP-F5GRAV0>
2024-03-01 08:53:48 -0500

    Solución Ejercicio_08

---
+++ Par_o_noesPar/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Se declaran variables
+        int Num;
+
+        //Se llama el Numero
+        Console.Write("Ingrese un Numero: ");
+        Num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        //Se valida si es par o no
+        if (Num % 2 == 0)
+        {
+            Console.WriteLine("Es un numero Par");
+        }
+        else
+        {
+            Console.WriteLine("Es un numero Impar");
+        }
+        Console.WriteLine();
+    }
+}
